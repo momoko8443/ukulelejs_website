10d21e4d8deee8908afa3a3c3a5ecbfc31c2b813
hbzheng <huibin.zheng@99wuxian.com>
2015-11-19 21:56:23 +0800

    up

--- pages/performance/bower_components/ukulelejs/src/ukulele/model/BoundItemBase.js
+++
@@ -1,9 +0,0 @@
-/**
- * @author Huibin
- */
-function BoundItemBase(attrName, element, uku) {
-    "use strict";
-    this.attributeName = attrName;
-    this.element = element;
-    this.uku = uku;
-}
