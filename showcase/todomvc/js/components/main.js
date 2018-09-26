0ef56a30648e44cf579885c30d7128023cc2f210
Jake.Zheng <Huibin.zheng@hpe.com>
2018-09-26 20:27:36 +0800

    update

--- showcase/todomvc/js/components/main.js
+++ showcase/todomvc/js/components/main.js
@@ -1,33 +1,27 @@
-/*jshint unused:false */
-(function () {
-	'use strict';
-	return function (uku) {
-		this.tasks = [];
-		this._isAllCompleted = false;
-		this.isShowToggleAllBtn = false;
-		this.toggleAllTodos = function () {
-			this._isAllCompleted = !this._isAllCompleted;
-			this.fire('toggleall', {
-				message: this._isAllCompleted
-			});
-		};
+this.tasks = [];
+this._isAllCompleted = false;
+this.isShowToggleAllBtn = false;
+this.toggleAllTodos = function () {
+	this._isAllCompleted = !this._isAllCompleted;
+	this.fire('toggleall', {
+		message: this._isAllCompleted
+	});
+};
 
-		Object.defineProperty(this, 'todos', {
-			set: function (value) {
-				if (value) {
-					this.tasks = value;
-				}
-			}
-		});
-		Object.defineProperty(this, 'showToggleAll', {
-			set: function (value) {
-				this.isShowToggleAllBtn = value;
-			}
-		});
-		Object.defineProperty(this, 'isAllCompleted', {
-			set: function (value) {
-				this._isAllCompleted = value;
-			}
-		});
-	};
-})();
+Object.defineProperty(this, 'todos', {
+	set: function (value) {
+		if (value) {
+			this.tasks = value;
+		}
+	}
+});
+Object.defineProperty(this, 'showToggleAll', {
+	set: function (value) {
+		this.isShowToggleAllBtn = value;
+	}
+});
+Object.defineProperty(this, 'isAllCompleted', {
+	set: function (value) {
+		this._isAllCompleted = value;
+	}
+});
